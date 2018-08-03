2a915120e1dc472b278209f7fe5d4c753520bd59
huangxz <huangxiaozheng@kedacom.com>
2018-08-03 16:10:10 +0800

    init

---
+++ t01/List_Tuple.py
@@ -0,0 +1,19 @@
+# print("中文str")
+#
+# print(ord('a'))
+#
+# print(chr(97))
+
+pList = ["htc", "sony", "android"]
+print(len(pList))
+
+pList.append("meizu")
+
+pList.insert(2, "xiaomi")
+
+print(pList[-4], len(pList), "\n", pList)
+
+print(pList.pop(), pList)
+
+mTuple = ("a", "b", "c")
+print("mTuple", "size:", len(mTuple))
